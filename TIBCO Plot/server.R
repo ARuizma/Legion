1349622c6ae56445d1ea1ea150e968389f2b2199
Aruma27 <andresrumarin@gmail.com>
2017-06-09 14:44:24 +0200

    no message

--- TIBCO Plot/server.R
+++ TIBCO Plot/server.R
@@ -1,16 +1,51 @@
 library(shiny)
+library(XLConnect)
+library(shinyjs)
+library(colourpicker)
+library(nlstools)
+library(ggplot2)
 
 
+ 
 server = function(input, output, session) {
- upfile <- reactive({
-  file1 <- input$file1
-  if (is.null(file1))
+ 
+ data <- reactive ({
+  infile <- input$file1
+  if(is.null(infile))
    return(NULL)
-  read.csv(file1$datapath, check.names=FALSE)
- })
- output$contents<-renderTable({upfile()})
+ 
+ df<- read.csv(infile$datapath, header=input$header, sep=input$sep, quote= input$quote)
+ 
  observe({
-  updateSelectInput(session, "inSelect", choices = names(upfile()))
+  updateSelectInput(session, 'xcol', choices = names(df), selected=names(df)[2])
+  updateSelectInput(session, 'ycol', choices = names(df), selected=names(df)[3])
+ })
+ 
+ return(df)
+ 
+ })
+ 
+ output$content<-renderTable({data()})
+  
+ 
+ #selectedData <- reactive({
+  #upfile[, c(input$xcol, input$ycol)]
+ #})
+ 
+ output$plot <- renderPlot({
+  
+  x <- data()[, c(input$xcol, input$ycol)]
+  switch(input$plot_scaletype,
+         normal = plot(x),
+         log =
+          plot(x, log = "xy", type = "l", main = "Curve Fitting")
+  )
   
  })
+ 
 }
+ 
+
+
+
+
--- TIBCO Plot/ui.R
+++ TIBCO Plot/ui.R
@@ -1,11 +1,80 @@
 
-ui = fluidPage(
- sidebarPanel(fileInput('file1', 'Choose CSV File',
+shinyUI(
+ 
+ fluidPage(
+  
+ #HeaderPanel####
+  
+  headerPanel(
+   strong("Legion")
+   #h2("We are Legion, for we are many")
+  ),
+  
+ #SIDEBAR####
+  
+  
+ sidebarPanel(fileInput('file1', 'Choose File with .csv format',
                         accept=c('text/csv', 
-                                 'text/tab-separated-values, text/comma-separated-values,text/plain', 
-                                 '.csv', '.txt', '.tsv')),
-              selectInput("inSelect", "Select something", choices = "Pending Upload")
+                                 'text/comma-separated-values, text/plain', 
+                                 '.csv')),
+              tags$br(),
+              checkboxInput('header', 'Header', TRUE),
+              radioButtons('sep', 'Separator',
+                           c(Comma=',',
+                             Semicolon=';',
+                             Tab='\t'),
+                           ','),
+              radioButtons('quote', 'Quote',
+                           c(None='',
+                             'Double Quote'='"',
+                             'Single Quote'="'"),
+                           '"'),
+              selectInput("plot_scaletype", "Scale type",
+                          c("normal" = "normal",
+                            "log" = "log"
+                          ),
+                          selected = "log",
+                          selectize = FALSE
+              ),
+              
+              selectInput('xcol', 'Select X variable', choices = "Pending Upload"),
+
+ 
+              selectInput('ycol', 'Select Y variable', choices = "Pending Upload")
  ),
+
+
+             
+ #MainPanel####
+ 
  mainPanel(
-  tableOutput("contents"))
+  tabsetPanel(
+   
+   tabPanel("Curve",
+            # Plot output
+             plotOutput('plot')
+   ),
+  
+   tabPanel("Summary",
+            withTags(					
+             div(class = "col-sm-12",
+                 h3(id="model-summary", "Model(s) summary", align="center"),
+                 tableOutput('summary')
+             )
+            )
+   ), 
+   
+   tabPanel("Content",
+            withTags(
+             div(class = "col-sm-12",
+                 h3(id="content", "Data Content", align= "center"),
+                 tableOutput('content')
+             )
+            )
+   )
+ )
+   
+ )
 )
+)
+
