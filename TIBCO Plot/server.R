128fa36cd752160632dc56727457bc31874a65bb
Aruma27 <andresrumarin@gmail.com>
2017-06-13 10:50:08 +0200

    no message

---
+++ TIBCO Plot/server.R
@@ -0,0 +1,53 @@
+library(shiny)
+library(XLConnect)
+library(shinyjs)
+library(colourpicker)
+library(nlstools)
+library(ggplot2)
+library(shinydashboard)
+ 
+server = function(input, output, session) {
+ 
+ data <- reactive ({
+  infile <- input$file1
+  if(is.null(infile))
+   return(NULL)
+ 
+ df<- read.csv(infile$datapath, header=input$header, sep=input$sep, quote= input$quote, check.names = FALSE)
+ 
+ observe({
+  x <- updateSelectInput(session, 'xcol', choices = names(df), selected=names(df)[2])
+  y <- updateSelectInput(session, 'ycol', choices = names(df), selected=names(df)[3])
+ })
+ 
+ return(df)
+ 
+ })
+ 
+ output$content<-renderTable({data()})
+  
+ 
+ #selectedData <- reactive({
+  #upfile[, c(input$xcol, input$ycol)]
+ #})
+ 
+ output$plot <- renderPlot({
+  
+  if(is.null(input$file1))
+     return(NULL)
+  
+  x <- data()[, c(input$xcol, input$ycol)]
+  switch(input$plot_scaletype,
+         normal = plot(x),
+         log =
+          plot(x, log = "xy", type = "p", main = "Curve Fitting")
+  )
+  
+ })
+ 
+}
+ 
+
+
+
+
