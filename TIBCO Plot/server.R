86c5fa3c5d8361454acdbb4b1f61d3463714dd57
Aruma27 <andresrumarin@gmail.com>
2017-06-21 10:30:16 +0200

    no message

--- TIBCO Plot/server.R
+++ TIBCO Plot/server.R
@@ -18,9 +18,9 @@ server = function(input, output, session) {
  df<- read.csv(infile$datapath, header=input$header, sep=input$sep, check.names = FALSE)
  
  observe({
-  z <- updateSelectInput(session, 'zcol', choices = names(df), selected=names(df)[1])
-  x <- updateSelectInput(session, 'xcol', choices = names(df), selected=names(df)[2])
-  y <- updateSelectInput(session, 'ycol', choices = names(df), selected=names(df)[3])
+ updateSelectInput(session, 'zcol', choices = names(df), selected=names(df)[1])
+ updateSelectInput(session, 'xcol', choices = names(df), selected=names(df)[2])
+ updateSelectInput(session, 'ycol', choices = names(df), selected=names(df)[3])
  })
  
  return(df)
@@ -29,29 +29,47 @@ server = function(input, output, session) {
  
  output$content<-renderTable({data()})
  
+ #NPLRTEST####
+ 
+ datalist <- reactive ({
+ if (is.null(infile))
+     return(NULL)
+ filelist <- split(infile, input$zcol)
+ })
+ 
+ conc <- input$xcol
+ y0 <- input$ycol
+ y1 <- convertToProp(y0)
+
+ 
+ test <- 
+ models <- lapply(datalist, function(tmp){
+  nplr(conc, y1, useLog = TRUE, LPweight = 0.25, npars="all", method = c("res", "sdw", "gw"), silent = FALSE)
+ })
  
  output$plot <- renderPlot({
   
   if(is.null(input$file1))
      return(NULL)
   
-  dat <- data()
+  plot(models)
+  #dat <- data()
   
-  switch(input$plot_scaletype,
+ # switch(input$plot_scaletype,
          
-         normal = ggplot(dat, aes_q(x=as.name(input$xcol), y=as.name(input$ycol), colour = as.name(input$zcol))) +
-          ggtitle("Curve Fitting") + theme(plot.title = element_text(hjust = 0.5)) +
-          geom_point() +
-          stat_summary(aes_q(y=as.name(input$ycol), group=as.name(input$zcol), colour = as.name(input$zcol)), fun.y = mean, geom = "line"),
+  #       normal = ggplot(dat, aes_q(x=as.name(input$xcol), y=as.name(input$ycol), colour = as.name(input$zcol))) +
+   #       ggtitle("Curve Fitting") + theme(plot.title = element_text(hjust = 0.5)) +
+    #      geom_point() +
+     #     stat_summary(aes_q(y=as.name(input$ycol), group=as.name(input$zcol), colour = as.name(input$zcol)), fun.y = mean, geom = "line"),
          
-         log =
-          ggplot(dat, aes_q(x=as.name(input$xcol), (y=as.name(input$ycol)), colour = as.name(input$zcol))) + 
-          ggtitle("Curve Fitting") + theme(plot.title = element_text(hjust = 0.5)) +
-          scale_x_log10() + 
-          geom_point() +
-          #geom_smooth(method = 'nls', formula = (y~a + ((b-a) / (1 + ((x / c) ^ d)))), method.args = list(start = c(a = "a2", b = "b2", c = "c2", d = "d2")), se = FALSE)
+      #   log =
+       #   ggplot(dat, aes_q(x=as.name(input$xcol), (y=as.name(input$ycol)), colour = as.name(input$zcol))) + 
+        #  ggtitle("Curve Fitting") + theme(plot.title = element_text(hjust = 0.5)) +
+         # scale_x_log10() + 
+          #geom_point() +
+          #geom_smooth(method = 'nplr', formula = , method.args = list(start = c(a = "a2", b = "b2", c = "c2", d = "d2")), se = FALSE)
           #stat_summary(aes_q(y=as.name(input$ycol), group=as.name(input$zcol), colour = as.name(input$zcol)), fun.y = mean, geom = "line")
-  )
+  #)
  })
  
  output$summary <- renderPrint({
@@ -59,6 +77,6 @@ server = function(input, output, session) {
   if(is.null(input$file1))
    return(NULL)
   
-  summary(data())
+  summary(models)
  })
 }
